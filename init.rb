988b1b6f1f8ac1de9e73420f63e9abecbadab84a
Bryan Chu <bryan.chu@ryerson.ca>
2015-10-01 17:39:07 -0400

    On branch version3x  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   init.rb

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 # Redmine Markdown Extra formatter
 require 'redmine'
 
-RAILS_DEFAULT_LOGGER.info 'Starting Markdown Extra formatter for RedMine'
+
+Rails.logger.info 'Starting Markdown Extra formatter for RedMine'
 
 Redmine::Plugin.register :redmine_markdown_extra_formatter do
   name 'Markdown Extra formatter'
@@ -9,5 +10,8 @@ Redmine::Plugin.register :redmine_markdown_extra_formatter do
   description 'This provides Markdown Extra as a wiki format'
   version '0.0.6'
 
-  wiki_format_provider 'Markdown Extra', RedmineMarkdownExtraFormatter::WikiFormatter, RedmineMarkdownExtraFormatter::Helper
+  wiki_format_provider 'Markdown Extra', RedmineMarkdownExtraFormatter::WikiFormatter
+
+
+  # , RedmineMarkdownExtraFormatter::Helper
 end
